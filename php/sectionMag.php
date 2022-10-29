66cc96e6bf91ef2919a89fa5ee7c2439cfca9979
unknown <giancarmelo.pittala@gmail.com>
2022-10-29 11:03:28 +0200

    test

--- php/sectionMag.php
+++ php/sectionMag.php
@@ -5,14 +5,10 @@ include 'ConnDB.php';
 $sql = "SELECT COD,DESCR FROM 01_desvar where tipo = 'D';";
 $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "<option value=". $row["COD"].">". $row["DESCR"]." </option>";
-    }
-} else {
-    echo "0 results SectionCat";
-}
+while($row = $result->fetch_assoc()) 
+    echo "<option value=". $row["COD"].">". $row["DESCR"]." </option>";
+    
+
 
 $conn->close();
 
