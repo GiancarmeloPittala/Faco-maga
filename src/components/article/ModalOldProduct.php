7183d31b8bad851cb06d5e9ec15925b2e83a3820
unknown <giancarmelo.pittala@gmail.com>
2022-11-08 19:47:04 +0100

    fix modal width

--- src/components/article/ModalOldProduct.php
+++ src/components/article/ModalOldProduct.php
@@ -1,11 +1,11 @@
 <div class="modal fade" id="modalOldProducts" aria-hidden="true" aria-labelledby="oldProducts" tabindex="-1">
   <div class="modal-dialog modal-dialog-centered ">
-    <div class="modal-dialog ">
+    <div class="modal-dialog w-100">
       <div class="modal-content bg-dark-sea-green">
         <div class="modal-header">
           <h5 data-bs-title="Seleziona un prodotto per ottenere i dati" data-bs-toggle="tooltip" data-bs-placement="top" title="Seleziona un prodotto per ottenere i dati" class="modal-title text-white">Lista prodotti</h5>
         </div>
-        <div class="modal-body" style="width: 400px">
+        <div class="modal-body" style="width: 100%">
           <div id="oldProductList" class="list-group">
 
           </div>
